22503327bbfd14453e11e5a6e55329644e7cb558
JamesJKrajicek <34218469+JamesJKrajicek@users.noreply.github.com>
2020-11-08 13:44:31 -0600

    Add Executive class.

---
+++ executive.js
@@ -0,0 +1,20 @@
+/**
+ * @class
+ */
+
+class Executive
+{
+    /**
+     * Specifies the number of rows and columns
+     * @constructor
+     */
+    constructor()
+    {
+        this.rows = 6;
+        this.cols = 7;
+    }
+
+
+
+
+}
