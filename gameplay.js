a6e156f7e2e57aebead9753841143ae9ceeeec1a
JamesJKrajicek <34218469+JamesJKrajicek@users.noreply.github.com>
2020-11-09 20:41:21 -0600

    Add gameboard event listener

--- gameplay.js
+++ gameplay.js
@@ -6,11 +6,11 @@
 
 function Gameplay ()
 {
-    this.board_rows = 6; //How many rows the board has.
-    this.board_cols = 7; //How many columns the board has.
+    board_rows = 6; //How many rows the board has.
+    board_cols = 7; //How many columns the board has.
     have_winner = false;
     player1_turn = true;
-    let play_board = new Board(this.board_rows, this.board_cols);
+    this.play_board = new Board(board_rows, board_cols);
 }
 
 /**
@@ -18,13 +18,28 @@ function Gameplay ()
  */
 Gameplay.prototype.run = function ()
 {
+    document.getElementById("game_board").addEventListener("click", e => {
+        
+        console.log("hi");
+        ;})
     while (!have_winner) //Gameplay loop
     {//Do Stuff
-
-        play_board.renderBoard();
-        
+        this.play_board.renderBoard();
+        document.getElementById("notifications").innerText = this.playersName() +"'s Turn";
 
         have_winner = true;
+    }
+}
+
+Gameplay.prototype.playersName = function ()
+{
+    if (player1_turn)
+    {
+        return "Player 1";
+    }
+    else
+    {
+        return "Player 2";
     }
 }
 
